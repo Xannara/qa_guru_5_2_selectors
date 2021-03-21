c09f8c8cfd7a099315ca2ff7ad13071a541902fc
Xannara <roksanasky@gmail.com>
2021-03-22 00:46:47 +0300

    добавлен gitignore, доработан тест

--- src/test/java/FillFormTests.java
+++ src/test/java/FillFormTests.java
@@ -17,44 +17,57 @@ public class FillFormTests {
 
     @Test
     void selenideFillFormTest() {
+
+        String firstName = "Name";
+        String lastName = "LastName";
+        String email = "name@example.com";
+        String gender = "Female";
+        String mobileNumber = "1920321016";
+        String subject = "Computer Science";
+        String picture = "first.png";
+        String address = "Test Address 234";
+        String state = "NCR";
+        String city = "Delhi";
+
         open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Name");
-        $("#lastName").setValue("LastName");
-        $("#userEmail").setValue("name@example.com");
-        $(byText("Female")).click();
-        $("#userNumber").setValue("1920321016");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(mobileNumber);
 
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("10");
         $(".react-datepicker__year-select").selectOptionByValue("1986");
         $(".react-datepicker__day--025").click();
 
-        $("#subjectsInput").setValue("Computer Science").pressEnter();
-        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#subjectsInput").setValue(subject).pressEnter();
 
         $("[for='hobbies-checkbox-1']").click();
-        $("#uploadPicture").uploadFromClasspath("first.png");
-        $("#currentAddress").setValue("Test Address 234");
+        $("#uploadPicture").uploadFromClasspath(picture);
+        $("#currentAddress").setValue(address);
 
         $("#state").click();
-        $(byText("NCR")).click();
+        $(byText(state)).click();
         $("#city").click();
-        $(byText("Delhi")).click();
+        $(byText(city)).click();
 
         $("#submit").click();
 
-        $(".modal-content").shouldBe(visible);
-        $(".modal-content").shouldHave(text("Name LastName"));
-        $(".modal-content").shouldHave(text("name@example.com"));
-        $(".modal-content").shouldHave(text("Female"));
-        $(".modal-content").shouldHave(text("1920321016"));
-        $(".modal-content").shouldHave(text("25 November,1986"));
-        $(".modal-content").shouldHave(text("Computer Science, Maths"));
-        $(".modal-content").shouldHave(text("Sports"));
-        $(".modal-content").shouldHave(text("first.PNG"));
-        $(".modal-content").shouldHave(text("Test Address 234"));
-        $(".modal-content").shouldHave(text("NCR Delhi"));
+        $(".modal-content").shouldBe(visible).shouldHave(text(firstName),
+                text(lastName),
+                text(email),
+                text(gender),
+                text(mobileNumber),
+                text("25 November,1986"),
+                text(subject),
+                text("Sports"),
+                text(picture),
+                text(address),
+                text(state),
+                text(city));
 
         $("#closeLargeModal").click();
+        $(".modal-content").shouldNotBe(visible);
     }
 }
